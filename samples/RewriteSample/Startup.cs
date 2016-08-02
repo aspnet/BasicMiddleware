87a30af0f6b3b30724638b0e402d216229afdbc5
Justin Kotalik <t-jukota@microsoft.com>
2016-08-02 16:37:22 -0700

    Fixing loading of project and copyright

--- samples/RewriteSample/Startup.cs
+++ samples/RewriteSample/Startup.cs
@@ -1,9 +1,11 @@
-﻿using System.IO;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Rewrite;
-using Microsoft.AspNetCore.Rewrite.UrlRewrite;
 
 namespace RewriteSample
 {
