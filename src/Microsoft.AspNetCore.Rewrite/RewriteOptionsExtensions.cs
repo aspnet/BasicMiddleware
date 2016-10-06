e6549d48f2f0fbdeb54010808a9f97b25de69bde
Mikael Mengistu <mimengis@microsoft.com>
2016-10-06 11:51:17 -0700

    Sorting usings

--- src/Microsoft.AspNetCore.Rewrite/RewriteOptionsExtensions.cs
+++ src/Microsoft.AspNetCore.Rewrite/RewriteOptionsExtensions.cs
@@ -2,8 +2,8 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using Microsoft.AspNetCore.Rewrite.Internal;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Rewrite.Internal;
 
 namespace Microsoft.AspNetCore.Rewrite
 {
