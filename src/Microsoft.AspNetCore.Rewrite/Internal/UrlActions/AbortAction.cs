21ebfdf66b02177ff39ad0137591bde00dd375b3
Mikael Mengistu <mimengis@microsoft.com>
2016-10-17 14:14:31 -0700

    Added support for the AbortRequest action

---
+++ src/Microsoft.AspNetCore.Rewrite/Internal/UrlActions/AbortAction.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Http;
+
+namespace Microsoft.AspNetCore.Rewrite.Internal.UrlActions
+{
+    public class AbortAction : UrlAction
+    {
+        public override void ApplyAction(RewriteContext context, MatchResults ruleMatch, MatchResults condMatch)
+        {
+            context.HttpContext.Abort();
+            context.Result = RuleResult.EndResponse;
+        }
+    }
+}
