2352613e48adc1bca3051bb72345b7836dd8edab
david-peden-q2 <david.peden@q2ebanking.com>
2016-12-05 16:19:08 -0600

    code review feedback

---
+++ src/Microsoft.AspNetCore.Rewrite/Internal/IISUrlRewrite/ServerVariableConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Microsoft.AspNetCore.Rewrite.Internal.IISUrlRewrite
+{
+    public static class ServerVariableConstants
+    {
+        public const string RequestHeaderPrefix = "HTTP_";
+        public const string ResponseHeaderPrefix = "RESPONSE_";
+    }
+}
