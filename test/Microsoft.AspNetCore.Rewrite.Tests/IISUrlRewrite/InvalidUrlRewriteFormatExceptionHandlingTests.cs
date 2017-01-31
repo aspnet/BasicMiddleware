7093fbd1640040686c91cb1ac565314486fc0aa0
david-peden-q2 <david.peden@q2ebanking.com>
2017-01-31 11:32:24 -0600

    coding standards

--- test/Microsoft.AspNetCore.Rewrite.Tests/IISUrlRewrite/InvalidUrlRewriteFormatExceptionHandlingTests.cs
+++ test/Microsoft.AspNetCore.Rewrite.Tests/IISUrlRewrite/InvalidUrlRewriteFormatExceptionHandlingTests.cs
@@ -1,7 +1,8 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
 using System.IO;
-
 using Microsoft.AspNetCore.Rewrite.Internal.IISUrlRewrite;
-
 using Xunit;
 
 namespace Microsoft.AspNetCore.Rewrite.Tests.UrlRewrite
